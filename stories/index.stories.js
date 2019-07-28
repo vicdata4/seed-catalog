a8a65ca687cbe28adc2660a168d4c1f4663105b4
vicdata <vicdata@outlook.com>
2019-07-28 20:22:54 +0200

    button suite

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,6 +6,25 @@ import { seedButtonStyle } from '../styles';
 storiesOf('Seed views', module)
   .add('BasicButton', () => html`<basic-button>Submit</basic-button>`)
   .add('Button style', () => html`
-      <style>${seedButtonStyle}</style>
-      <button class="sd-btn green">Hellow</button>
+      <style>
+        ${seedButtonStyle}
+
+        div {
+          padding: 10px;
+          min-height: 200px;
+        }
+      </style>
+      <p style="color:white;">Button suite</p>
+      <div style="background-color: #292929;">
+      <button class="sd-btn">Submit</button>
+      <button class="sd-btn red">Home</button>
+      <button class="sd-btn green">About us</button>
+      <button class="sd-btn blue">Contact</button>
+      </div>
+      <div style="background-color: #ededed;">
+      <button class="sd-btn">Submit</button>
+      <button class="sd-btn red">Home</button>
+      <button class="sd-btn green">About us</button>
+      <button class="sd-btn blue">Contact</button>
+      </div>
   `);
--- styles.js
+++ styles.js
@@ -1,13 +1,54 @@
 import { css } from 'lit-element';
 
 export const seedButtonStyle = css`
+
+    @import url('https://fonts.googleapis.com/css?family=Heebo&display=swap');
+
     .sd-btn {
-        background-color: blue;
-        border: 3px solid black;
-        color: red;
+        font-family: 'Heebo', sans-serif !important;
+        background-color: white;
+        font-weight: bolder;
+        font-size: 1rem;
+        font-weight: 300;
+        margin: 2px;
+        padding: 12px;
+        color: #3b3b3b;
+        border: none;
+        border-radius: 5px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        cursor: pointer;
+        transition: background-color .2s;
+    }
+
+    .sd-btn:hover {
+        background-color: #dedede;
+    }
+
+    .red {
+        background-color: #de3d4c;
+        color: white;
+    }
+
+    .red:hover {
+        background-color: #bb3440;
     }
 
     .green {
-        background-color: green;
+        background-color: #6ea230;
+        color: white;
+    }
+
+    .green:hover {
+        background-color: #5e8b28;
+    }
+
+    .blue {
+        background-color: #2dabb4;
+        color: white;
+    }
+
+    .blue:hover {
+        background-color: #239aa3;
     }
 `;
