94c5f1325704ac9e719176cff686ca02feb77cb6
vicdata <vicdata@outlook.com>
2019-08-04 23:21:11 +0200

    first buttons

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,30 +1,137 @@
-import { storiesOf, html } from '@open-wc/demoing-storybook';
+import { storiesOf } from '@open-wc/demoing-storybook';
+import { html } from 'lit-html';
 
 import '../components/basic-button';
 import { seedButtonStyle } from '../styles';
 
-storiesOf('Seed views', module)
-  .add('BasicButton', () => html`<basic-button>Submit</basic-button>`)
-  .add('Button style', () => html`
-      <style>
-        ${seedButtonStyle}
+storiesOf('Seed Library', module)
+  //.add('BasicButton', () => html`<basic-button>Submit</basic-button>`)
+  .add('Buttons', () => html`
+    <style>
+      ${seedButtonStyle}
 
-        div {
-          padding: 10px;
-          min-height: 200px;
-        }
-      </style>
-      <p style="color:white;">Button suite</p>
-      <div style="background-color: #292929;">
+      .container {
+        padding: 10px;
+        min-height: 100px;
+      }
+
+      h2, p {
+        color: red;
+      }
+
+      h2:hover, h5:hover {
+        cursor: unset;
+        background-color: none;
+      }
+
+      .light {
+        color: #e4bb05;
+        font-size: 1.5rem;
+      }
+
+      .decorator {
+        color: #e4bb05;
+      }
+
+      a {
+        color: #989898;
+        text-decoration: none;
+      }
+    </style>
+    <h2 class="sd-btn black">Class <span class="light">sd-btn</span> for ${'<button> && <a>'} tags</h2>
+    <h5 class="sd-btn black">${'<button class="'}<span style="color: #e4bb05">sd-btn red</span>${'">Home</button>'}</h5>
+    <h5 class="sd-btn black">${'<a class="'}<span style="color: #e4bb05">sd-btn blue</span>${'">Home</a>'}</h5>
+    <div class="container" style="background-color: #292929;">
       <button class="sd-btn">Submit</button>
-      <button class="sd-btn red">Home</button>
-      <button class="sd-btn green">About us</button>
-      <button class="sd-btn blue">Contact</button>
-      </div>
-      <div style="background-color: #ededed;">
+      <button class="sd-btn red">Red</button>
+      <button class="sd-btn green">Green</button>
+      <button class="sd-btn blue">Blue</button>
+      <button class="sd-btn yellow">Yellow</button>
+      <button class="sd-btn purple">Purple</button>
+      <button class="sd-btn black">Black</button>
+      <button class="sd-btn orange">Orange</button>
+    </div>
+    <div class="container" style="background-color: #ededed;">
       <button class="sd-btn">Submit</button>
-      <button class="sd-btn red">Home</button>
-      <button class="sd-btn green">About us</button>
-      <button class="sd-btn blue">Contact</button>
+      <button class="sd-btn red">Close</button>
+      <button class="sd-btn green">Green</button>
+      <button class="sd-btn blue">Blue</button>
+      <a href="/" class="sd-btn yellow">Yellow</a>
+      <button class="sd-btn purple">Purple</button>
+      <button class="sd-btn black">Black</button>
+      <button class="sd-btn orange">Orange</button>
+    </div>
+    <h2 class="sd-btn black">Disabled buttons</h2>
+    <div class="container">
+      <h5 class="sd-btn black">${'<button class="sd-btn" '}<span class="decorator">disabled</span>${'>Home</button>'}</h5>
+      <button class="sd-btn" disabled>Submit</button>
+      <button class="sd-btn red" disabled>Red</button>
+      <button class="sd-btn green" disabled>Green</button>
+      <button class="sd-btn blue" disabled>Blue</button>
+    </div>
+    <h2 class="sd-btn black">Buttons with icons</h2>
+    <div class="container" style="background-color: #292929;">
+      
+      <h5 class="sd-btn black"><span class="decorator">Fa Icons </span> <a href="https://fontawesome.com/v4.7.0/examples/" target="_blank">Fa Documentation</a></h5>
+      <h5 class="sd-btn black">${'<button class="sd-btn red">'}<span class="decorator">${'<i class="fa fa-close"></i>'}</span>${'</button>'}</h5>
+      <h5 class="sd-btn black">${'<button class="sd-btn red '}<span class="decorator">circle</span>${'">'}<span class="decorator">${'<i class="fa fa-close"></i>'}</span>${'</button>'}</h5>
+      <button class="sd-btn red circle"><i class="fa fa-close"></i></button>
+      <button class="sd-btn yellow circle"><i class="fa fa-key"></i></button>
+      <button class="sd-btn blue circle"><i class="fa fa-folder"></i></button>
+      <button class="sd-btn green"><i class="fa fa-folder"></i></button>
+      <button class="sd-btn green circle"><i class="fa fa-arrow-right"></i></button>
+      <button class="sd-btn blue circle"><i class="fa fa-bars"></i></button>
+
+      <button class="sd-btn red lg"><i class="fa fa-close"></i></button>
+      <button class="sd-btn green lg circle"><i class="fa fa-home"></i></button>
+      <button class="sd-btn blue lg circle"><i class="fa fa-user"></i></button>
+
+      <button class="sd-btn red sm"><i class="fa fa-close"></i></button>
+      <button class="sd-btn green sm circle"><i class="fa fa-home"></i></button>
+      <button class="sd-btn blue sm"><i class="fa fa-bars"></i></button>
       </div>
+      
+      <h5 class="sd-btn black"><span class="decorator">Material Icons </span><a href="https://material.io/resources/icons/?style=baseline" target="_blank">Material Icons Documentation</a></h5>
+      <h5 class="sd-btn black">${'<button class="sd-btn">'}<span class="decorator">${'<i class="material-icons">close</i>'}</span>${'</button>'}</h5>
+      <button class="sd-btn red circle"><i class="material-icons">close</i></button>
+      <button class="sd-btn green"><i class="material-icons">home</i></button>
+      <button class="sd-btn green circle"><i class="material-icons">chevron_right</i></button>
+      <button class="sd-btn green yellow circle"><i class="material-icons">expand_more</i></button>
+      <button class="sd-btn blue circle"><i class="material-icons">menu</i></button>
+
+      <button class="sd-btn red lg"><i class="material-icons">face</i></button>
+      <button class="sd-btn green lg circle"><i class="material-icons">people</i></button>
+      <button class="sd-btn blue lg"><i class="material-icons">menu</i></button>
+
+      <button class="sd-btn red sm"><i class="material-icons">close</i></button>
+      <button class="sd-btn green sm circle"><i class="material-icons">home</i></button>
+      <button class="sd-btn blue sm"><i class="material-icons">menu</i></button>
+      <button class="sd-btn blue sm circle"><i class="material-icons">face</i></button>
+      <br/><br/><br/><br/>
+      <h2 class="sd-btn black">Class <span class="light">sd-btn-mix</span> for ${'buttons with icons and text'}. <span class="decorator">Support fa and material icons</span></h2>
+      <div>
+      <h5 class="sd-btn black"><span class="decorator">Fa Icons</span></h5>
+      <h5 class="sd-btn black">${'<button class="'}<span class="decorator">sd-btn-mix red</span>${'">'}<span class="decorator">${'<i class="fa fa-close"></i>'}</span>${'Cancel payment</button>'}</h5>
+      <button class="sd-btn-mix red sm"><i class="fa fa-close"></i>Cancel payment</button>
+      <button class="sd-btn-mix red"><i class="fa fa-close"></i>Cancel payment</button>
+      <button class="sd-btn-mix red lg"><i class="fa fa-close"></i>Cancel payment</button>
+      <button class="sd-btn-mix blue"><i class="fa fa-folder"></i>Folders</button>
+      <button class="sd-btn-mix blue"><i class="fa fa-spinner fa-spin fa-fw"></i>Downloading</button>
+
+      <h5 class="sd-btn black"><span class="decorator">Material Icons </span></h5>
+      <h5 class="sd-btn black">${'<button class="'}<span class="decorator">sd-btn-mix red</span>${'">'}<span class="decorator">${'<i class="material-icons">close</i>'}</span>${'Cancel payment</button>'}</h5>
+      
+      <button class="sd-btn-mix red sm"><i class="material-icons">close</i>Cancel payment</button>
+      <button class="sd-btn-mix red"><i class="material-icons">close</i>Cancel payment</button>
+      <button class="sd-btn-mix red lg"><i class="material-icons">close</i>Cancel payment</button>
+      <button class="sd-btn-mix yellow"><i class="material-icons">warning</i>Warning message</button>
+      <button class="sd-btn-mix black"><i class="material-icons">lock</i>Lock account</button>
+      <br/><br/><br/><br/>
+      <h2 class="sd-btn black">Class <span class="light">sd-btn-mixr</span> </h2>
+      <h5 class="sd-btn black">${'<button class="'}<span class="decorator">sd-btn-mixr red</span>${'">'}Download files<span class="decorator">${'<i class="fa fa-cloud-download"></i>'}</span>${'</button>'}</h5>
+      <button class="sd-btn-mixr green sm">Download files<i class="fa fa-cloud-download"></i></button>
+      <button class="sd-btn-mixr green">Download files<i class="fa fa-cloud-download"></i></button>
+      <button class="sd-btn-mixr green lg">Download files<i class="fa fa-cloud-download"></i></button>
+      
+    </div>
   `);
--- styles.js
+++ styles.js
@@ -3,52 +3,116 @@ import { css } from 'lit-element';
 export const seedButtonStyle = css`
 
     @import url('https://fonts.googleapis.com/css?family=Heebo&display=swap');
-
-    .sd-btn {
-        font-family: 'Heebo', sans-serif !important;
+    @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+    @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+    /*@import '/font-awesome-4.7.0/css/font-awesome.css';*/
+    /*** DEFAULT - WHITE ***/
+    .sd-btn, .sd-btn-mix, .sd-btn-mixr {
+        font-family: 'Heebo', sans-serif;
         background-color: white;
         font-weight: bolder;
         font-size: 1rem;
         font-weight: 300;
         margin: 2px;
-        padding: 12px;
+        padding: .6rem 1rem;
+        /*padding: .380rem .80rem;*/
         color: #3b3b3b;
         border: none;
         border-radius: 5px;
-        padding-top: 5px;
-        padding-bottom: 5px;
         cursor: pointer;
-        transition: background-color .2s;
+        text-decoration: none;
+        transition: background-color .1s;
+        line-height: 1;
     }
 
-    .sd-btn:hover {
-        background-color: #dedede;
+    .sd-btn > i {
+        vertical-align: middle;
     }
 
-    .red {
-        background-color: #de3d4c;
-        color: white;
+    .fa {
     }
 
-    .red:hover {
-        background-color: #bb3440;
+
+    .sd-btn-mix > * { margin-right: 7px; }
+    .sd-btn-mixr > * { margin-left: 7px; }
+
+    .sd-btn:focus { outline: 4px solid #ffffff40; }
+    .sd-btn:disabled { opacity: .8; }
+    .sd-btn:hover:enabled { background-color: #dedede; }
+
+    /*** RED ***/
+    .red { background-color: #de3d4c; color: white; }
+    .red:focus { outline: 4px solid #ff00184d; }
+    .red:hover:enabled { background-color: #bb3440; }
+
+    /*** GREEN ***/
+    .green { background-color: #22ac41; color: white; }
+    .green:focus { outline: 4px solid #7fc72a45; }
+    .green:hover:enabled { background-color: #199936; }
+
+    /*** BLUE ***/
+    .blue { background-color: #2dabb4; color: white; }
+    .blue:focus { outline: 4px solid #2dabb445; }
+    .blue:hover:enabled { background-color: #239aa3; }
+
+    /*** YELLOW ***/
+    .yellow { background-color: #e6af0e; color: #434343;}
+    .yellow:focus { outline: 4px solid #c89a0c3d; }
+    .yellow:hover:enabled { background-color: #d5a927; }
+
+    /*** PURPLE ***/
+    .purple { background-color: #903e8d; color: white; }
+    .purple:focus { outline: 4px solid #903e8d5e; }
+    .purple:hover:enabled { background-color: #663564; }
+
+    /*** BLACK ***/
+    .black { background-color: #2f2d2d; color: white; }
+    .black:focus { outline: 4px solid #43434387; }
+    .black:hover:enabled { background-color: #434343; }
+
+    /*** ORANGE ***/
+    .orange { background-color: #d58309; color: white;}
+    .orange:focus { outline: 4px solid #e09e2057; }
+    .orange:hover:enabled { background-color: #b46e06;}
+
+
+    
+
+   
+    
+
+    .sd-btn.lg > .fa {
+        vertical-align: baseline;
     }
 
-    .green {
-        background-color: #6ea230;
-        color: white;
+    .sm { font-size: .8rem; padding: .200rem .50rem; }
+    .lg { font-size: 1.7rem; padding: .200rem .80rem; }
+
+    .sm > .material-icons { font-size: .9rem; font-weight: bold; }
+    .lg > .material-icons { font-size: 2rem; }
+
+    .circle {
+        border-radius: 100%;
+        padding: .3rem .3rem;
+        width: 2.1rem;
+        height: 2.1rem;
     }
 
-    .green:hover {
-        background-color: #5e8b28;
+    .circle.sm {
+        width: 1.7rem;
+        height: 1.7rem;
     }
 
-    .blue {
-        background-color: #2dabb4;
-        color: white;
+    .circle.lg {
+        width: 3rem;
+        height: 3rem;
     }
 
-    .blue:hover {
-        background-color: #239aa3;
+    .circle.lg > .material-icons {
+        vertical-align: sub;
     }
+
+
+
+    
 `;
