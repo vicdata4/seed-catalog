658750d8688811824a32bbb5881af4c4a4581a4e
victorgh <victor.gonzalez@soprasteria.com>
2019-10-12 17:13:04 +0200

    materials

--- styles.js
+++ styles.js
@@ -1,9 +1,33 @@
 import { css } from 'lit-element';
 
-import './fonts/material-icons/iconfont/material-icons.css';
-import './fonts/font-awesome/css/font-awesome.css';
+const materials = css`
+    /* fallback */
+    @font-face {
+    font-family: 'Material Icons';
+    font-style: normal;
+    font-weight: 400;
+    src: url(https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2) format('woff2');
+    }
 
+    .material-icons {
+    font-family: 'Material Icons';
+    font-weight: normal;
+    font-style: normal;
+    font-size: 24px;
+    line-height: 1;
+    letter-spacing: normal;
+    text-transform: none;
+    display: inline-block;
+    white-space: nowrap;
+    word-wrap: normal;
+    direction: ltr;
+    -webkit-font-feature-settings: 'liga';
+    -webkit-font-smoothing: antialiased;
+    }
+
+`;
 export const seedButtonStyle = css`
+${materials}
 /*
     @import url('https://fonts.googleapis.com/css?family=Heebo&display=swap');
     /*** DEFAULT - WHITE ***/
