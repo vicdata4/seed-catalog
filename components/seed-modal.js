ef73998fa120905dccf776a23b650339e167a556
victorgh <victor.gonzalez@soprasteria.com>
2019-10-19 13:20:14 +0200

    modal position: fixed

--- components/seed-modal.js
+++ components/seed-modal.js
@@ -7,11 +7,11 @@ export class SeedModal extends LitElement {
             seedButtonStyle,
             css`
             .modal {
+                position: fixed;
                 display: flex;
                 align-items: center;
                 justify-content: center;
 
-                position: absolute;
                 top: 0;
                 bottom: 0;
                 right: 0;
