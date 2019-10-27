fc3dd3771cfa14074887e4f9e458e9a5525fcb60
victorgh <victor.gonzalez@soprasteria.com>
2019-10-27 17:43:28 +0100

    seed dropdown/collapse

--- components/seed-dropdown.js
+++ components/seed-dropdown.js
@@ -6,27 +6,48 @@ export class SeedDropdown extends LitElement {
         return [
             seedButtonStyle,
             css`
+
+                :host {
+                    display: inline-block;
+                }
+
                 .dropdown {
                     position: relative;
                     height: 0;
+                    max-height: 0;
                     overflow: hidden;
+                    transition: height .2s ease-in-out;
                     z-index: 999;
-                    transition: height .5s linear;
+                    font-size: 15px;
+                    
+                }
+
+                ::slotted(div) {
+                    display: flex;
+                    flex-direction: column;
+                }
+
+                ::slotted(p) {
+                    margin: 0;
+                    padding: 15px;
+                }
+
+                
+
+                .show {
+                    height: auto;
+                    width: auto;
+                    background-color: white;
+                    max-height: 600px;
                 }
 
                 .hide {
                     height: 0;
+                    max-height: 0;
                 }
 
-                .show {
-                    height: auto;
-                    background-color: white;
-                    width: 300px;
-                    padding: 10px; 
-                }
-
-                a {
-                    color: black;
+                .house {
+                    max-width: 250px;
                 }
         `]
     }
@@ -34,29 +55,56 @@ export class SeedDropdown extends LitElement {
     static get properties() {
         return {
             modalBackground: { type: String },
+            rotate: { type: String },
+            width: { type: String },
+            position: { type: String },
+            mode: { type: String },
         };
     }
 
     constructor() {
         super();
+        this.rotate = this.rotate || false;
+        this.position = this.position || 'absolute';
+        this.mode = this.mode || 'default';
     }
 
-    firstUpdated() {}
+    firstUpdated() {
+        let dropdown = this.shadowRoot.querySelector('.dropdown');
+        let button = this.shadowRoot.querySelector('slot').assignedNodes()[0];
+        const rotate = this.rotateIcon.bind(this);
+        document.addEventListener('click', function(e){
+            if ((e.target.tagName !== 'BUTTON' || e.target.id !== button.id) && e.target.className !== 'content') {
+                    dropdown.className = 'dropdown hide';
+                    rotate('0');
+            } else if(e.target === this){
+                dropdown.className = 'dropdown show'; 
+            }
+        });
+    }
+
+    rotateIcon(value) {
+        if(this.rotate) {
+            this.shadowRoot.querySelector('slot').assignedNodes()[0].querySelector('i').style.transition = 'transform .2s ease-in';
+            this.shadowRoot.querySelector('slot').assignedNodes()[0].querySelector('i').style.transform = `rotate(${value}deg)`;
+        }
+    }
 
     openCollapse() {
-        const classx = this.shadowRoot.querySelector('.dropdown').className;
-        
+        let classx = this.shadowRoot.querySelector('.dropdown').className;
         if(classx === 'dropdown show') {
             this.shadowRoot.querySelector('.dropdown').className = 'dropdown hide';
+            this.rotateIcon('0');    
         }else {
             this.shadowRoot.querySelector('.dropdown').className = 'dropdown show';
+            this.rotateIcon('180');
         }
     }
 
     render() {
         return html`
             <slot name="button" @click="${this.openCollapse}"></slot>
-            <div class="dropdown">
+            <div class="dropdown" style="position: ${this.position}; transition: max-height ${this.mode === 'collapse' ? '.5' : '0'}s ease-in-out;">
                 <slot name="content"></slot>
             </div>
         `;
--- stories/dropdown.stories.js
+++ stories/dropdown.stories.js
@@ -5,26 +5,98 @@ import { seedButtonStyle } from '../styles';
 import '../components/seed-dropdown';
 
 storiesOf('Seed Library', module)
-  .add('Collapse', () => html`
+  .add('Dropdown', () => html`
         <style>
             ${seedButtonStyle}
+
+            * {
+                font-family: 'Ubuntu', sans-serif;
+            }
+            .dropdown-links > a {
+                color: #483f3f;
+                text-decoration: none;
+                
+                display: flex;
+                align-items: center;
+                padding: 8px;
+                padding-left: 20px;
+                padding-right: 50px;
+            }
+
+            .dropdown-links > a:hover {
+                background-color: #f9f9f9;
+            }
+
+            .dropdown-links > a > i{
+                margin-right: 15px;
+            }
         </style>
   
-        <seed-dropdown>
-            <button slot="button" class="sd-btn-mix red"><i class="material-icons">keyboard_arrow_down</i>Open collapse</button>
-            <div slot="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut viverra leo, vel dapibus quam. Proin a sollicitudin quam, eget viverra diam. Donec euismod mattis dignissim. Fusce convallis lacus enim, eget ultricies neque tristique vel. Fusce vehicula, elit id ultrices dignissim, nibh mauris feugiat justo, a fermentum velit diam at enim.
+       
+        <br>
+        <h2 class="sd-btn black sm">position fixed (overflow)</h2>
+        <br><br>
+        <seed-dropdown rotate="true">
+            <button id="restaurant" slot="button" class="sd-btn-mix red-mate"><i class="material-icons">arrow_drop_down</i>Menu</button>
+            <div slot="content" class="dropdown-links">
+                <a href="/">Select type</a>
+                <a href="/"><i class="material-icons green-mate sm">fastfood</i>Burgers</a>
+                <a href="/"><i class="material-icons green-mate sm">local_dining</i>Salads</a>
+                <a href="/"><i class="material-icons green-mate sm">local_cafe</i>Desserts</a>
+                <a href="/"><i class="material-icons red sm">local_bar</i>Drinks</a>
             </div>
         </seed-dropdown>
 
-        <seed-dropdown>
-        <button slot="button" class="sd-btn-mix blue">Link</button>
-        <div slot="content">
-            <a href="#">Hola</a>
-            <a href="#">Hola</a>
-            <a href="#">Hola</a>
-            <a href="#">Hola</a>
-            <a href="#">Hola</a>
-        </div>
+         <seed-dropdown mode="collapse" rotate="true">
+            <button id="paragrap" slot="button" class="sd-btn-mix black"><i class="material-icons">keyboard_arrow_down</i>Collapse mode</button>
+            <p slot="content" class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut viverra leo, vel dapibus quam. Proin a sollicitudin quam, eget viverra diam. Donec euismod mattis dignissim. Fusce convallis lacus enim, eget ultricies neque tristique vel. Fusce vehicula, elit id ultrices dignissim, nibh mauris feugiat justo, a fermentum velit diam at enim.
+            </p>
         </seed-dropdown>
+
+        <seed-dropdown>
+            <button id="lorem" slot="button" class="sd-btn-mix green"><i class="material-icons">local_library</i>Default dropdown</button>
+            <p slot="content" class="content">
+            Space Exploration Technologies Corp., doing business as SpaceX, is a private American aerospace manufacturer and space transportation services company headquartered in Hawthorne, California. It was founded in 2002 by Elon Musk with the goal of reducing space transportation costs to enable the colonization of Mars.[7][8][9] SpaceX has developed the Falcon launch vehicle family and the Dragon spacecraft family.
+            </p>
+        </seed-dropdown>
+
+        <seed-dropdown>
+            <button id="menu" slot="button" class="sd-btn-mix blue-mate">Dropwdown Links</button>
+            <div slot="content" class="content dropdown-links">
+                <a href="/"><i class="material-icons blue-mate sm">home</i>Home</a>
+                <a href="/"><i class="material-icons blue-mate sm">info</i>About us</a>
+                <a href="/"><i class="material-icons blue-mate sm">web</i>Products</a>
+                <a href="/"><i class="material-icons blue-mate sm">shop</i>Shop</a>
+                <a href="/"><i class="material-icons blue-mate sm">contact_mail</i>Contact</a>
+            </div>
+        </seed-dropdown>
+        <br>
+        <h2 class="sd-btn black sm">position relative ACCORDION EXAMPLE</h2>
+        <br><br>
+        <seed-dropdown mode="collapse" position="relative" rotate="true">
+            <button id="lol" slot="button" class="sd-btn-mix black"><i class="material-icons">keyboard_arrow_down</i>LitElement</button>
+            <p slot="content" class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut viverra leo, vel dapibus quam. Proin a sollicitudin quam, eget viverra diam. Donec euismod mattis dignissim. Fusce convallis lacus enim, eget ultricies neque tristique vel. Fusce vehicula, elit id ultrices dignissim, nibh mauris feugiat justo, a fermentum velit diam at enim.
+            </p>
+        </seed-dropdown>
+        <seed-dropdown mode="collapse" position="relative" rotate="true">
+            <button id="lock" slot="button" class="sd-btn-mix black"><i class="material-icons">keyboard_arrow_down</i>Redux</button>
+            <p slot="content" class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut viverra leo, vel dapibus quam. Proin a sollicitudin quam, eget viverra diam. Donec euismod mattis dignissim. Fusce convallis lacus enim, eget ultricies neque tristique vel. Fusce vehicula, elit id ultrices dignissim, nibh mauris feugiat justo, a fermentum velit diam at enim.
+            </p>
+        </seed-dropdown>
+        <seed-dropdown mode="collapse" position="relative" rotate="true">
+            <button id="drot" slot="button" class="sd-btn-mix black"><i class="material-icons">keyboard_arrow_down</i>Vue</button>
+            <p slot="content" class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut viverra leo, vel dapibus quam. Proin a sollicitudin quam, eget viverra diam. Donec euismod mattis dignissim. Fusce convallis lacus enim, eget ultricies neque tristique vel. Fusce vehicula, elit id ultrices dignissim, nibh mauris feugiat justo, a fermentum velit diam at enim.
+            </p>
+        </seed-dropdown>
+        <seed-dropdown mode="collapse" position="relative" rotate="true">
+            <button id="lops" slot="button" class="sd-btn-mix black"><i class="material-icons">keyboard_arrow_down</i>Angular</button>
+            <p slot="content" class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut viverra leo, vel dapibus quam. Proin a sollicitudin quam, eget viverra diam. Donec euismod mattis dignissim. Fusce convallis lacus enim, eget ultricies neque tristique vel. Fusce vehicula, elit id ultrices dignissim, nibh mauris feugiat justo, a fermentum velit diam at enim.
+            </p>
+        </seed-dropdown>
+
     `);
