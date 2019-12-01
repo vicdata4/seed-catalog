ba35ad6454ab8c2329b06d1ad4409cfc5a20822c
victorgh <victor.gonzalez@soprasteria.com>
2019-12-01 16:39:30 +0100

    refactor(seed-collapse): new collapse utils file from components/utils directory

--- components/seed-collapse.js
+++ components/seed-collapse.js
@@ -1,12 +1,5 @@
 import { LitElement, html, css } from 'lit-element';
-import { MAX_DROP_HEIGHT } from './utils/constants';
-
-const collapseTransition = params => {
-  const { dropdown, transition, height, maxHeight } = params;
-  dropdown.style.transition = transition;
-  dropdown.style.height = height;
-  dropdown.style.maxHeight = maxHeight;
-};
+import { collapseUtils } from './utils/collapse';
 
 export class SeedCollapse extends LitElement {
   static get styles() {
@@ -29,72 +22,6 @@ export class SeedCollapse extends LitElement {
   }
 
   /**
-   * collapseAccordionList()
-   *
-   * This function close opened dropdowns and open closed dropdowns
-   * in order to get the accordion effect
-   *
-   * @param {Array} list seed-dropdown list
-   * @param {Tag} element seed-dropdown > .dropdown
-   * @param {String} time (max-with 5s)
-   * @param {String} type (ease-in-out, linear)
-   */
-
-  collapseAccordionList(list, element, time, type) {
-    list.forEach(component => {
-      const dropdown = component.shadowRoot.querySelector('.dropdown');
-      const isClosed = element === dropdown;
-      const isOpened = dropdown.style.maxHeight === MAX_DROP_HEIGHT;
-
-      if (isOpened) {
-        collapseTransition({
-          dropdown,
-          transition: `${time} cubic-bezier(0, .6, 0, 1)`,
-          height: 'unset',
-          maxHeight: '0'
-        });
-        component.rotateIcon('0');
-      }
-
-      if (isClosed) {
-        collapseTransition({
-          dropdown,
-          transition: `${time} ${type}`,
-          height: 'auto',
-          maxHeight: MAX_DROP_HEIGHT
-        });
-        component.rotateIcon('180');
-      }
-    });
-  }
-
-  /**
-   * setCollapse()
-   *
-   * @param {Array} list seed-dropdown list
-   * @param {String} time (max-with 5s)
-   * @param {String} type (ease-in-out, linear)
-   */
-
-  setCollapse(list, type, time) {
-    this.shadowRoot.addEventListener('set-collapse', component => {
-      const dropdown = component.target.shadowRoot.querySelector('.dropdown');
-      const isOpened = dropdown.style.maxHeight === MAX_DROP_HEIGHT;
-      if (isOpened) {
-        collapseTransition({
-          dropdown,
-          transition: `${time} cubic-bezier(0, 1, 0, 1)`,
-          height: 'unset',
-          maxHeight: '0'
-        });
-        component.target.rotateIcon('0');
-      } else {
-        this.collapseAccordionList(list, dropdown, time, type);
-      }
-    });
-  }
-
-  /**
    * getDropdownList()
    *
    * Return dropdown-list from collapse component
@@ -113,22 +40,29 @@ export class SeedCollapse extends LitElement {
 
   /**
    *
-   * Get dropdownList() and set transition and speed according to list length
+   * Get dropdown list and set collapse property to true.
+   * Call to collapseUtils() with predefined type and speed
    */
 
-  collapseDropdown() {
+  setCollapseUtils() {
     const list = this.getDropdownList();
 
     if (list.length > 0) {
       list.forEach(x => { x.collapse = true; });
-      const transitionType = list.length > 1 ? 'linear' : 'ease-in-out';
-      const transitionSpeed = list.length > 1 ? '1' : '.8';
-      this.setCollapse(list, transitionType, `max-height ${transitionSpeed}s`);
+      this.shadowRoot.addEventListener('set-collapse', component => {
+        const manyDropdowns = list.length > 1;
+        collapseUtils({
+          component,
+          list,
+          type: manyDropdowns ? 'linear' : 'ease-in-out',
+          time: `max-height ${manyDropdowns ? '1' : '.8'}s`
+        });
+      });
     }
   }
 
   firstUpdated() {
-    this.collapseDropdown();
+    this.setCollapseUtils();
   }
 
   render() {
---
+++ components/utils/collapse.js
@@ -0,0 +1,80 @@
+import { MAX_DROP_HEIGHT } from './constants';
+
+/**
+   *
+   * collapseTransition()
+   * Set .dropdown transition values
+   * @param {Object} params Transition attributes
+   */
+
+const collapseTransition = params => {
+  const { dropdown, transition, height, maxHeight } = params;
+  dropdown.style.transition = transition;
+  dropdown.style.height = height;
+  dropdown.style.maxHeight = maxHeight;
+};
+
+/**
+   * collapseAccordionList()
+   *
+   * This function close opened dropdowns and open closed dropdowns
+   * in order to get the accordion effect
+   *
+   * @param {Array} list seed-dropdown list
+   * @param {Tag} element seed-dropdown > .dropdown
+   * @param {String} time (max-with 5s)
+   * @param {String} type (ease-in-out, linear)
+   */
+
+const collapseAccordionList = (list, element, time, type) => {
+  list.forEach(component => {
+    const dropdown = component.shadowRoot.querySelector('.dropdown');
+    const isClosed = element === dropdown;
+    const isOpened = dropdown.style.maxHeight === MAX_DROP_HEIGHT;
+
+    if (isOpened) {
+      collapseTransition({
+        dropdown,
+        transition: `${time} cubic-bezier(0, .6, 0, 1)`,
+        height: 'unset',
+        maxHeight: '0'
+      });
+      component.rotateIcon('0');
+    }
+
+    if (isClosed) {
+      collapseTransition({
+        dropdown,
+        transition: `${time} ${type}`,
+        height: 'auto',
+        maxHeight: MAX_DROP_HEIGHT
+      });
+      component.rotateIcon('180');
+    }
+  });
+};
+
+/**
+   * collapseUtils()
+   *
+   * @param {Object} component seed-dropdown
+   * @param {Array} list
+   * @param {String} time (max-with 5s)
+   * @param {String} type (ease-in-out, linear)
+   */
+
+export const collapseUtils = ({ component, list, time, type }) => {
+  const dropdown = component.target.shadowRoot.querySelector('.dropdown');
+  const isOpened = dropdown.style.maxHeight === MAX_DROP_HEIGHT;
+  if (isOpened) {
+    collapseTransition({
+      dropdown,
+      transition: `${time} cubic-bezier(0, 1, 0, 1)`,
+      height: 'unset',
+      maxHeight: '0'
+    });
+    component.target.rotateIcon('0');
+  } else {
+    collapseAccordionList(list, dropdown, time, type);
+  }
+};
