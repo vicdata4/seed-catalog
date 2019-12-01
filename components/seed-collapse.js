f49de9828dfca5d65b3d322dd5fa4e50b70f76dc
victorgh <victor.gonzalez@soprasteria.com>
2019-12-01 02:02:23 +0100

    refactor(seed-collapse): refactor

--- components/seed-collapse.js
+++ components/seed-collapse.js
@@ -21,62 +21,65 @@ export class SeedCollapse extends LitElement {
     };
   }
 
-  setParams(element, transition, height, maxHeight, icon, rotate) {
+  addTransition(element, transition, height, maxHeight, icon, rotate) {
     element.style.transition = transition;
     element.style.height = height;
     element.style.maxHeight = maxHeight;
     icon.rotateIcon(rotate);
   }
 
-  listIterator(list, element, time, type) {
+  dropdownListIterator(list, element, time, type) {
     list.forEach(x => {
       const dropdown = x.shadowRoot.querySelector('.dropdown');
       if (dropdown.style.maxHeight === MAX_DROP_HEIGHT || element === dropdown) {
-        this.setParams(dropdown, `max-height ${time}s cubic-bezier(0, .6, 0, 1)`, 'unset', '0', x, '0');
+        this.addTransition(dropdown, `max-height ${time}s cubic-bezier(0, .6, 0, 1)`, 'unset', '0', x, '0');
         if (element === dropdown) {
-          this.setParams(dropdown, `max-height ${time}s ${type}`, 'auto', MAX_DROP_HEIGHT, x, '180');
+          this.addTransition(dropdown, `max-height ${time}s ${type}`, 'auto', MAX_DROP_HEIGHT, x, '180');
         }
       }
     });
   }
 
-  setCollapse(list, type, time_) {
+  setCollapse(list, type, time) {
     this.shadowRoot.addEventListener('set-collapse', e => {
       const element = e.target.shadowRoot.querySelector('.dropdown');
-      const time = time_;
       const cubicTransition = `max-height ${time}s cubic-bezier(0, 1, 0, 1)`;
 
       if (element.style.maxHeight === MAX_DROP_HEIGHT) {
-        this.setParams(element, cubicTransition, 'unset', '0', e.target, '0');
+        this.addTransition(element, cubicTransition, 'unset', '0', e.target, '0');
       } else {
-        this.listIterator(list, element, time, type);
+        this.dropdownListIterator(list, element, time, type);
       }
     });
   }
 
-  firstUpdated() {
-    let list = [];
+  collapseDropdown() {
+    const list = this.getDropdownList();
+
+    if (list.length > 0) {
+      list.forEach(x => { x.collapse = true; });
+      const transitionType = list.length > 1 ? 'linear' : 'ease-in-out';
+      const transitionSpeed = list.length > 1 ? '1' : '.8';
+
+      this.setCollapse(list, transitionType, transitionSpeed);
+    }
+  }
+
+  getDropdownList() {
     const dropdownList = this.querySelectorAll('seed-dropdown');
     let slottedList = this.querySelector('slot');
     slottedList = (slottedList) ? slottedList.assignedElements() : [];
 
-    if (dropdownList.length > 0) list = dropdownList;
-    if (slottedList.length > 0) list = slottedList;
+    if (dropdownList.length > 0) return dropdownList;
+    if (slottedList.length > 0) return slottedList;
+    return [];
+  }
 
-    if (list.length === 1) {
-      list[0].collapse = true;
-      this.setCollapse(list, 'ease-in-out', '.8');
-    } else {
-      if (this.accordion && list.length > 0) {
-        list.forEach(x => { x.collapse = true; });
-      }
-      this.setCollapse(list, 'linear', '1');
-    }
+  firstUpdated() {
+    this.collapseDropdown();
   }
 
   render() {
-    return html`
-      <slot></slot>
-    `;
+    return html`<slot></slot>`;
   }
 }
