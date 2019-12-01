beb06c586373d161b4f38c5ebc649525412e2061
victorgh <victor.gonzalez@soprasteria.com>
2019-12-01 13:59:47 +0100

    refactor(seed-collapse): addTransition()

--- components/seed-collapse.js
+++ components/seed-collapse.js
@@ -1,6 +1,22 @@
 import { LitElement, html, css } from 'lit-element';
 import { MAX_DROP_HEIGHT } from './utils/constants';
 
+const addTransition = params => {
+  const {
+    dropdown,
+    component,
+    transition,
+    height = 'unset',
+    maxHeight = '0',
+    rotate = '0'
+  } = params;
+
+  dropdown.style.transition = transition;
+  dropdown.style.height = height;
+  dropdown.style.maxHeight = maxHeight;
+  component.rotateIcon(rotate);
+};
+
 export class SeedCollapse extends LitElement {
   static get styles() {
     return [
@@ -21,34 +37,50 @@ export class SeedCollapse extends LitElement {
     };
   }
 
-  addTransition(element, icon, transition, height, maxHeight, rotate) {
-    element.style.transition = transition;
-    element.style.height = height;
-    element.style.maxHeight = maxHeight;
-    icon.rotateIcon(rotate);
-  }
-
-  dropdownListIterator(list, element, time, type) {
-    list.forEach(x => {
-      const dropdown = x.shadowRoot.querySelector('.dropdown');
-      if (dropdown.style.maxHeight === MAX_DROP_HEIGHT || element === dropdown) {
-        this.addTransition(dropdown, x, `max-height ${time}s cubic-bezier(0, .6, 0, 1)`, 'unset', '0', '0');
-        if (element === dropdown) {
-          this.addTransition(dropdown, x, `max-height ${time}s ${type}`, 'auto', MAX_DROP_HEIGHT, '180');
+  dropdownListIterator(list, element_, time, type) {
+    list.forEach(component => {
+      const dropdown = component.shadowRoot.querySelector('.dropdown');
+      if (dropdown.style.maxHeight === MAX_DROP_HEIGHT || element_ === dropdown) {
+        addTransition({
+          dropdown,
+          component,
+          transition: `max-height ${time}s cubic-bezier(0, .6, 0, 1)`
+        });
+        if (element_ === dropdown) {
+          addTransition({
+            dropdown,
+            component,
+            transition: `max-height ${time}s ${type}`,
+            height: 'auto',
+            maxHeight: MAX_DROP_HEIGHT,
+            rotate: '180'
+          });
         }
       }
     });
   }
 
   setCollapse(list, type, time) {
-    this.shadowRoot.addEventListener('set-collapse', element => {
-      const dropdown = element.target.shadowRoot.querySelector('.dropdown');
+    this.shadowRoot.addEventListener('set-collapse', element_ => {
+      const dropdown = element_.target.shadowRoot.querySelector('.dropdown');
       if (dropdown.style.maxHeight === MAX_DROP_HEIGHT) {
-        this.addTransition(dropdown, element.target, `max-height ${time}s cubic-bezier(0, 1, 0, 1)`, 'unset', '0', '0');
+        addTransition({
+          dropdown,
+          component: element_.target,
+          transition: `max-height ${time}s cubic-bezier(0, 1, 0, 1)`
+        });
       } else {
         this.dropdownListIterator(list, dropdown, time, type);
       }
     });
+  }
+
+  getDropdownList() {
+    const dropdownList = this.querySelectorAll('seed-dropdown');
+    let slottedList = this.querySelector('slot');
+    slottedList = (slottedList) ? slottedList.assignedElements() : [];
+
+    return (dropdownList.length > 0) ? dropdownList : (slottedList.length > 0) ? slottedList : [];
   }
 
   collapseDropdown() {
@@ -60,14 +92,6 @@ export class SeedCollapse extends LitElement {
       const transitionSpeed = list.length > 1 ? '1' : '.8';
       this.setCollapse(list, transitionType, transitionSpeed);
     }
-  }
-
-  getDropdownList() {
-    const dropdownList = this.querySelectorAll('seed-dropdown');
-    let slottedList = this.querySelector('slot');
-    slottedList = (slottedList) ? slottedList.assignedElements() : [];
-
-    return (dropdownList.length > 0) ? dropdownList : (slottedList.length > 0) ? slottedList : [];
   }
 
   firstUpdated() {
