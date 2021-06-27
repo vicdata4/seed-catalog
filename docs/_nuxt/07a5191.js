1aca6c8a4fb406989373439662d85ddfa8c14bf2
vicdata <vicdata@outlook.com>
2021-06-27 22:40:30 +0200

    deploy website

--- docs/_nuxt/07a5191.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{242:function(e,t,n){"use strict";n.r(t);var l={layout:"seed"},r=n(11),component=Object(r.a)(l,(function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)}),[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("h1",[e._v("Buttons")])])}],!1,null,null,null);t.default=component.exports}}]);
