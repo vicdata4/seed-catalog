3bf9abb04046237656c2496cc20302e4a9dc994f
vicdata <vicdata@outlook.com>
2021-06-27 15:50:24 +0200

    Updated deploy config

--- docs/_nuxt/109fbb7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{209:function(n,o,t){"use strict";t.r(o),t.d(o,"pageInit",(function(){return r}));var e=t(211),c=t.n(e),r=(t(212),t(213),t(214),t(215),t(216),t(217),t(218),t(219),t(220),t(221),function(){document.querySelector(".section-container").scrollTo({top:0,behavior:"smooth"}),c.a.highlightAll()})}}]);
---
+++ docs/_nuxt/6b4e3ad.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{211:function(n,o,t){"use strict";t.r(o),t.d(o,"pageInit",(function(){return r}));var e=t(213),c=t.n(e),r=(t(214),t(215),t(216),t(217),t(218),t(219),t(220),t(221),t(222),t(223),function(){document.querySelector(".section-container").scrollTo({top:0,behavior:"smooth"}),c.a.highlightAll()})}}]);
